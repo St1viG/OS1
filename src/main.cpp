0597558272e003eab7ed5595cc51ede133a58a8f
St1viG <stevan.gnjato@gmail.com>
2025-10-04 11:54:51 -0700

    blasphemy

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 #include "../h/print.hpp"
 #include "../h/riscv.hpp"
 #include "../h/MemoryAllocator.hpp"
-#include "../h/syscall_c.hpp"
 #include "../test/printing.hpp"
 #include "../test/buffer.hpp"
 
 
 extern void userMain();
 _sem* waitUsr = nullptr;
+Buffer* buffer;
 
 void userWrapper(void* arg){
     printString("user main start\n");
@@ -24,12 +24,11 @@ int main()
 {
 
     MemoryAllocator::initialise();
+    buffer = new Buffer(300);
 
-//    TCB::init();
-//    Riscv::init();
     Riscv::w_stvec((uint64)&Riscv::supervisorTrap);
     Riscv::ms_sstatus(Riscv::SSTATUS_SIE);
-//    Riscv::ms_sie(Riscv::SIE_SEIE);
+
     sem_open(&waitUsr,0);
     thread_t* user= nullptr;
 
@@ -39,9 +38,7 @@ int main()
 
     sem_wait(waitUsr);
 
-//    while(Buffer::getCnt()>0) { TCB::dispatch(); }
+    while(buffer->getCnt()>0) { TCB::dispatch(); }
     Riscv::mc_sstatus(Riscv::SSTATUS_SIE);
-//    stop_emulator();
-
     return 0;
 }
